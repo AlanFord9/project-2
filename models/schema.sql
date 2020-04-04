7c3deacf4557b66e5b17896fb014039fd6d7b4f1
pattenbranden <patten.branden@gmail.com>
2020-04-04 11:25:26 -0400

    added post block to index.html

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,5 @@
 
-DROP DATABASE IF EXISTS blogger;
-CREATE DATABASE blogger;
+DROP DATABASE IF EXISTS corona_blog;
+CREATE DATABASE corona_blog;
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,26 @@
-// var db = require("../models");
+var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  // app.get("/", function(req, res) {
-  //   db.Example.findAll({}).then(function(dbExamples) {
-  //     res.render("index", {
-  //       msg: "Welcome!",
-  //       examples: dbExamples
-  //     });
-  //   });
-  // });
-  // // Load example page and pass in an example by id
-  // app.get("/user/:id", function(req, res) {
-  //   db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
-  //     res.render("example", {
-  //       example: dbExample
-  //     });
-  //   });
-  // });
-  // // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   res.render("404");
-  // });
+  app.get("/", function(req, res) {
+    db.Posts.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        posts: dbExamples
+      });
+      console.log("rendering index");
+    });
+  });
+  // Load example page and pass in an example by id
+  app.get("/user/:id", function(req, res) {
+    db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
+      res.render("example", {
+        // example: dbExample
+      });
+    });
+  });
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
 };
